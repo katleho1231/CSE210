d037c84433c735516ff2cd099ebac5318deaadf5
katleho1231 <selwanakatleho@gmail.com>
2026-03-21 14:58:40 +0200

    message

---
+++ week03/ScriptureMemorizer/scripture.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+
+        string[] parts = text.Split(" ");
+        foreach (string part in parts)
+        {
+            _words.Add(new Word(part));
+        }
+    }
+
+    public void HideRandomWords(int count)
+    {
+        Random rand = new Random();
+        int hidden = 0;
+
+        while (hidden < count)
+        {
+            int index = rand.Next(_words.Count);
+
+            if (!_words[index].IsHidden())
+            {
+                _words[index].Hide();
+                hidden++;
+            }
+        }
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public string GetDisplayText()
+    {
+        string text = "";
+
+        foreach (Word word in _words)
+        {
+            text += word.GetDisplayText() + " ";
+        }
+
+        return _reference.GetDisplayText() + " " + text;
+    }
+}
